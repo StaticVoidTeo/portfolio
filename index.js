5e3e9e1a08d77b7cb9f924a8d7182ec243ac013a
Teo <kingteo692@gmail.com>
2025-10-28 13:21:22 +0100

    Updating the entire portfolio - remake

--- index.js
+++ index.js
@@ -1,67 +1,89 @@
-let menuItems = document.querySelector(".header").querySelectorAll(".menuItem");
-let arrowLeft = document.querySelector("#leftArrow");
-let arrowRight = document.querySelector("#rightArrow");
-let contactBtn = document.querySelector("#contactBtn");
-let devH = document.querySelector("#developer");
-let cursorDot = document.querySelector("[data-cursor-dot]");
-let cursorOutline = document.querySelector("[data-cursor-outline]");
-let menuIcon = document.querySelector(".menuIcon");
-let xIcon = document.querySelector(".xIcon");
+const menuItems = document.querySelector("#header").querySelectorAll(".menuItem");
+const portfolioBtn = document.querySelector("#portfolioBtn");
+const devH = document.querySelector("#developer");
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+const menuIcon = document.querySelector(".menuIcon");
+const xIcon = document.querySelector(".xIcon");
 
 let prevSec = document.querySelector(`.section[value='1']`);
 let prevMenuItem = document.querySelector(`.menuItem[value='1']`);
 prevMenuItem.id = "selectedMenuItem";
 let val = 1;
-let wheelCounter = 1;
-arrowLeft.style = "display:none;";
 let currentDev = "";
 let dev = "Developer"; let devI = 0; let holdDev = 8;let eraseDev = false;
-let dontScroll = false;
-let prevVal;
-
-function bottomVisible(){
-    if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight - 1)
-        return true;
-    return false;
-}
-
-function topVisible(){
-    if(window.scrollY == 0){
-        return true;
-    }
-    return false;
-}
-
 let prevScroll = 0;
 
+const windowSize = window.innerWidth; // checks window size to know if it is mobile or desktop
+if(windowSize <= 768)
+    document.querySelector("#header").classList.add("mobile-actual-header");
+else
+    document.querySelector("#header").classList.add("desktop-header");
+
 window.addEventListener("scroll", () => {
-    if(document.querySelector(".header").id == "mobile-header")
+    if(document.querySelector("#header").classList.contains("mobile-header"))
         return;
     if(prevScroll < window.scrollY){
-        document.querySelector(".header").id = "up";
+        document.querySelector("#header").classList.remove("down");
+        document.querySelector("#header").classList.add("up");
     }
     else{
-        document.querySelector(".header").id = "down";
+        document.querySelector("#header").classList.remove("up");
+        document.querySelector("#header").classList.add("down");
     }
     prevScroll = window.scrollY;
 })
 
 menuIcon.addEventListener("click", () => {
-    document.querySelector(".header").id = "mobile-header";
-    document.querySelector(".header img").style = "display:none";
-    menuIcon.style = "display:none";
-    xIcon.style = "display:flex";
+    document.querySelector("#header").classList.remove("down");
+    document.querySelector("#header").classList.remove("up");
+    document.querySelector("#header").classList.remove("mobile-actual-header");
+    document.querySelector("#header").classList.add("mobile-header");
 })
 
 xIcon.addEventListener("click", () => {
-    document.querySelector(".header").id = "mobile-header-slideup";
+    document.querySelector("#header").classList.add("mobile-header-slideup");
     setTimeout(() => {
-        document.querySelector(".header img").style = "display:default;";
-        menuIcon.style = "display:flex";
-        xIcon.style = "display:none";
-        document.querySelector(".header").id = "down";
+        document.querySelector("#header").classList.remove("mobile-header");
+        document.querySelector("#header").classList.remove("mobile-header-slideup");
+        document.querySelector("#header").classList.add("mobile-actual-header");
+        document.querySelector("#header").classList.add("down");
     }, 701)
 })
+
+portfolioBtn.addEventListener("click", () => {
+    prevSec.style = "display:none;";
+    prevMenuItem.id = "";
+    val = 4;
+    prevMenuItem = document.querySelector(`.menuItem[value="4"]`);
+    prevMenuItem.id = "selectedMenuItem";
+    prevSec = document.querySelector(`.section[value='4']`);
+    prevSec.style = "display:flex;";
+})
+
+menuItems.forEach((el) => {
+    el.addEventListener("click", () => {
+        if(windowSize <= 768){
+            document.querySelector("#header").classList.add("mobile-header-slideup");
+            setTimeout(() => {
+                document.querySelector("#header").classList.remove("mobile-header-slideup");
+                document.querySelector("#header").classList.remove("mobile-header");
+                document.querySelector("#header").classList.add("mobile-actual-header");
+                document.querySelector("#header").classList.add("down");
+            }, 701)
+        }
+
+        val = el.getAttribute("value");
+        prevSec.style = "display:none;";
+        prevMenuItem.id = "";
+        el.id = "selectedMenuItem";
+        prevMenuItem = el;
+        document.querySelector(`.section[value='${val}']`).style = "display:flex;";
+        prevSec = document.querySelector(`.section[value='${val}']`);
+    })
+});
+
+//---------------------------------------------------------------------------------------------------------------------------------
 
 window.addEventListener("mousemove", (e) => {
     let posX = e.clientX;
@@ -100,150 +122,4 @@ setInterval(() => {
         }
         devH.innerHTML = currentDev + "_";
     }
-}, 100)
-
-contactBtn.addEventListener("click", () => {
-    arrowRight.style = "display:none;";
-    prevSec.style = "display:none;";
-    prevMenuItem.id = "";
-    val = 4;
-    if(window.innerWidth > 755)
-        arrowLeft.style = "display:block;";
-    document.querySelector(`.menuItem[value="4"]`).id = "selectedMenuItem";
-    prevMenuItem = document.querySelector(`.menuItem[value="4"]`);
-    document.querySelector(`.section[value="4"`).style =
-    "display:flex;";
-    document.querySelector(`.section[value="4"`).className = "section slideFromRight";
-    prevVal = val;
-    prevSec = document.querySelector(`.section[value='4']`);
-    wheelCounter = 1;
-})
-
-menuItems.forEach((el) => {
-    el.addEventListener("click", () => {
-        if(window.innerWidth <= 755){
-            document.querySelector(".header").id = "mobile-header-slideup";
-            setTimeout(() => {
-                document.querySelector(".header img").style = "display:default;";
-                menuIcon.style = "display:flex";
-                xIcon.style = "display:none";
-                document.querySelector(".header").id = "down";
-            }, 701)
-        }
-
-        val = el.getAttribute("value");
-        if(window.innerWidth > 755){
-        if(val == 1){
-            arrowLeft.style = "display:none;";
-            arrowRight.style = "display:block;";
-        }
-        else if(val == 4){
-            arrowRight.style = "display:none;";
-            arrowLeft.style = "display:block;";
-        }
-        else{
-            arrowRight.style = "display:block;";
-            arrowLeft.style = "display:block;";
-        }}
-        prevSec.style = "display:none;";
-        prevMenuItem.id = "";
-        document.querySelector(`.menuItem[value="${val}"]`).id = "selectedMenuItem";
-        prevMenuItem = document.querySelector(`.menuItem[value="${val}"]`);
-        document.querySelector(`.section[value='${val}']`).style =
-        "display:flex;";
-        if(prevVal > val)
-            document.querySelector(`.section[value='${val}']`).className = "section slideFromLeft";
-        else
-            document.querySelector(`.section[value='${val}']`).className = "section slideFromRight";
-        prevVal = val;
-        prevSec = document.querySelector(`.section[value='${val}']`);
-        wheelCounter = 1;
-    })
-})
-
-arrowLeft.addEventListener("click", () => {
-    arrowRight.style = "display:block;";
-    prevSec.style = "display:none;";
-    prevMenuItem.id = "";
-    val--;
-    if(window.innerWidth > 755)
-        if(val == 1)
-            arrowLeft.style = "display:none;";
-    document.querySelector(`.menuItem[value="${val}"]`).id = "selectedMenuItem";
-    prevMenuItem = document.querySelector(`.menuItem[value="${val}"]`);
-    document.querySelector(`.section[value="${val}"`).style =
-    "display:flex;";
-    document.querySelector(`.section[value="${val}"`).className = "section slideFromLeft";
-    prevVal = val;
-    prevSec = document.querySelector(`.section[value='${val}']`);
-    wheelCounter = 1;
-})
-
-arrowRight.addEventListener("click", () => {
-    arrowLeft.style = "display:block;";
-    prevSec.style = "display:none;";
-    prevMenuItem.id = "";
-    val++;
-    if(window.innerWidth > 755)
-        if(val == 4)
-            arrowRight.style = "display:none;";
-    document.querySelector(`.menuItem[value="${val}"]`).id = "selectedMenuItem";
-    prevMenuItem = document.querySelector(`.menuItem[value="${val}"]`);
-    document.querySelector(`.section[value="${val}"`).style =
-    "display:flex;";
-    document.querySelector(`.section[value="${val}"`).className = "section slideFromRight";
-    prevVal = val;
-    prevSec = document.querySelector(`.section[value='${val}']`);
-    wheelCounter = 1;
-})
-
-window.addEventListener("wheel", function(event) {
-    if(dontScroll)
-        return;
-    if(!bottomVisible() && event.deltaY > 0){
-        return;
-    }
-    if(!topVisible() && event.deltaY < 0){
-        return;
-    }
-    if(val == 4 && event.deltaY > 0){
-        wheelCounter = 1;
-        return;
-    }
-    else if(val == 1 && event.deltaY < 0){
-        wheelCounter = 1;
-        return;
-    }
-    if(wheelCounter <= 4){
-        wheelCounter++;
-        return;
-    }
-    prevSec.style = "display:none;";
-    prevMenuItem.id = "";
-    let inc = (event.deltaY > 0) ? 1 : -1;
-    val = parseInt(val) + inc;
-    if(window.innerWidth > 755){
-    if(val == 1){
-        arrowLeft.style = "display:none;";
-        arrowRight.style = "display:block;";
-    }
-    else if(val == 4){
-        arrowRight.style = "display:none;";
-        arrowLeft.style = "display:block;";
-    }
-    else{
-        arrowRight.style = "display:block;";
-        arrowLeft.style = "display:block;";
-    }}
-    document.querySelector(`.menuItem[value="${val}"]`).id = "selectedMenuItem";
-    prevMenuItem = document.querySelector(`.menuItem[value="${val}"]`);
-    document.querySelector(`.section[value="${val}"`).style =
-    "display:flex;";
-    if(prevVal > val)
-        document.querySelector(`.section[value='${val}']`).className = "section slideFromLeft";
-    else
-        document.querySelector(`.section[value='${val}']`).className = "section slideFromRight";
-    prevVal = val;
-    prevSec = document.querySelector(`.section[value='${val}']`);
-    wheelCounter = 1;
-})
+}, 100);
